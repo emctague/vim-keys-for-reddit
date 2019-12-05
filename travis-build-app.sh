5f91fcc73ddf56234ec2b0e8421d63b7c0b64ad6
Ethan McTague <ethan@tague.me>
2019-12-05 16:38:34 -0500

    Try to get shit working AGAIN

--- travis-build-app.sh
+++ travis-build-app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xcodebuild archive -scheme "Vim Keys for Reddit" -archivePath vkfr-archive CODE_SIGNING_REQUIRED=NO SWIFT_VERSION=5
+xcodebuild archive -scheme "Vim Keys for Reddit" -archivePath vkfr-archive CODE_SIGNING_REQUIRED=NO SWIFT_VERSION=5 CODE_SIGNING_IDENTITY="travis_dist_key.p12"
 xcodebuild -archivePath vkfr-archive.xcarchive -exportArchive -exportOptionsPlist ExportOptions.plist -exportPath .
 zip Vim.Keys.for.Reddit.zip "Vim Keys for Reddit.app"
