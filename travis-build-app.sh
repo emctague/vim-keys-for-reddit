15ed03af871e4a2dd992d4d0e54a47ad875e278e
Ethan McTague <ethan@tague.me>
2019-12-05 16:23:37 -0500

    Try to fix code signing... again...

--- travis-build-app.sh
+++ travis-build-app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xcodebuild archive -scheme "Vim Keys for Reddit" -archivePath vkfr-archive
+xcodebuild archive -scheme "Vim Keys for Reddit" -archivePath vkfr-archive CODE_SIGNING_REQUIRED=NO
 xcodebuild -archivePath vkfr-archive.xcarchive -exportArchive -exportOptionsPlist ExportOptions.plist -exportPath .
 zip Vim.Keys.for.Reddit.zip "Vim Keys for Reddit.app"
