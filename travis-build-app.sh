c42652f76de5a75bc81501402b7f65271c55c508
Ethan McTague <ethan@tague.me>
2019-12-05 16:06:22 -0500

    Fix travis build scheme

--- travis-build-app.sh
+++ travis-build-app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xcodebuild archive -archivePath vkfr-archive
+xcodebuild archive -scheme "Vim Keys for Reddit" -archivePath vkfr-archive
 xcodebuild -archivePath vkfr-archive.xcarchive -exportArchive -exportOptionsPlist ExportOptions.plist -exportPath .
 zip Vim.Keys.for.Reddit.zip "Vim Keys for Reddit.app"
