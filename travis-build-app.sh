1d7617c1092852c3e5bb3709d91d3d4f1e1c87c8
Ethan McTague <ethan@tague.me>
2019-12-05 21:42:22 -0500

    Maybe fix things?

--- travis-build-app.sh
+++ travis-build-app.sh
@@ -3,7 +3,7 @@
 security create-keychain -p travis mac-build.keychain
 security default-keychain -s mac-build.keychain
 security unlock-keychain -p travis mac-build.keychain
-security import ./travis_dist_key.p12 -k mac-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./travis_dist_key.p12 -k mac-build.keychain -P "$KEY_PASSWORD" -T /usr/bin/codesign
 
 xcodebuild archive -scheme "Vim Keys for Reddit" -archivePath vkfr-archive CODE_SIGNING_REQUIRED=NO SWIFT_VERSION=5
 xcodebuild -archivePath vkfr-archive.xcarchive -exportArchive -exportOptionsPlist ExportOptions.plist -exportPath .
